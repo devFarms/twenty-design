449753b36c29af53e614d0d7414150e7ad0dc9c6
Alex Green <agreen@santa-ana.org>
2018-02-08 14:33:46 -0800

    Album Art refreshes using button clicks.

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,17 +1,65 @@
-function getAlbumArt(){
+
+
+// $("button").on("click", function(){
+//     var btnUserName = $(this).attr('data-name');
+//     console.log(btnUserName);
+//     if (btnUserName === topTwentyUsers[0])
+// });
+
+function getAlbumArtAlex(){
+    var albumArt = $('#album-art-banner').text('');
     axios.get('https://api.spotify.com/v1/users/wouldratherusegoogleplay/playlists/52x0gyrdCKPxtR2cDuscKz/tracks?market=US&fields=items&limit=20&offset=0', {
     headers: {
         "Accept":"application/json",
         "Content-Type":"application/json",
-        "Authorization":"Bearer BQAFFcHuRO-_XvQ8sq1s51Bd9mcMaukI6MDlFwMV2vxI-j70MmB-XduJOmpGam6prvvFBwM27e2McRt2kfbNmnlYHYn7f3vWsp5K5AbQ6jPVIaqahUwwXdn4YBGC_aWtteWA3DQEgdRWwg8qhb5sfOSQL4LGoO4pVPg"
+        "Authorization":"Bearer BQBv7fqlPnzphFgjBirhRpyXP6OJDZNTw3KTZeWbz0EFJCNDt-dxIxHOMFhbGIjALjq7V_Y6ofoirSyaYCqCKk2bbE6SEhTrF3zr50I6Mj-V16BLv0l2onVNoEBiztTVNuT48oyo4-VtWnYwQ4LsZic4f0RkwJRF7PU"
     }
     }).then(function (response){
-        // console.log(response.data.items[0].track.album.images[1].url);
         for (i = 0; i < response.data.items.length; i++){
-            // console.log(response.data.items[i].track.album.images[1].url);
+            albumArt = $('<img>');
+            albumArt.attr("src", response.data.items[i].track.album.images[1].url);
+            $('#album-art-banner').append(albumArt);
+            console.log("Album Art function executed.");
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+function getAlbumArtAnita(){
+    var albumArt = $('#album-art-banner').text('');
+    axios.get('https://api.spotify.com/v1/users/digitallobe/playlists/47RtQFMzYZJXTsUoGwk0G9/tracks?market=US&fields=items&limit=20&offset=0', {
+    headers: {
+        "Accept":"application/json",
+        "Content-Type":"application/json",
+        "Authorization":"Bearer BQBv7fqlPnzphFgjBirhRpyXP6OJDZNTw3KTZeWbz0EFJCNDt-dxIxHOMFhbGIjALjq7V_Y6ofoirSyaYCqCKk2bbE6SEhTrF3zr50I6Mj-V16BLv0l2onVNoEBiztTVNuT48oyo4-VtWnYwQ4LsZic4f0RkwJRF7PU"
+    }
+    }).then(function (response){
+        for (i = 0; i < response.data.items.length; i++){
             var albumArt = $('<img>');
             albumArt.attr("src", response.data.items[i].track.album.images[1].url);
             $('#album-art-banner').append(albumArt);
+            console.log("Album Art function executed.");
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+function getAlbumArtSara(){
+    var albumArt = $('#album-art-banner').text('');
+    axios.get('https://api.spotify.com/v1/users/digitallobe/playlists/0GhPfY9MvQtnuwMHd7pVtu/tracks?market=US&fields=items&limit=20&offset=0', {
+    headers: {
+        "Accept":"application/json",
+        "Content-Type":"application/json",
+        "Authorization":"Bearer BQBv7fqlPnzphFgjBirhRpyXP6OJDZNTw3KTZeWbz0EFJCNDt-dxIxHOMFhbGIjALjq7V_Y6ofoirSyaYCqCKk2bbE6SEhTrF3zr50I6Mj-V16BLv0l2onVNoEBiztTVNuT48oyo4-VtWnYwQ4LsZic4f0RkwJRF7PU"
+    }
+    }).then(function (response){
+        for (i = 0; i < response.data.items.length; i++){
+            var albumArt = $('<img>');
+            albumArt.attr("src", response.data.items[i].track.album.images[1].url);
+            $('#album-art-banner').append(albumArt);
+            console.log("Album Art function executed.");
         }
     }).catch(function (error) {
         console.log(error);
